1ff8b5ac56dac126fc32df822002f2a99dceea41
Fedor Naumenko <30726965+fnaumenko@users.noreply.github.com>
2020-08-04 17:34:30 +0100

    Update freqCall.h

--- freqCall.h
+++ freqCall.h
@@ -5,28 +5,56 @@
 #include <queue>
 
 using namespace std;
+typedef __int64 LLONG;
 
-const char LF = '\n';
+// 'Point' represents event <time><value> 
+struct Point {
+	float tm;
+	float vl;
+
+	inline Point(float t = 0, float v = 0) : tm(t), vl(v) {}
+
+	// for sorting by descend
+	inline bool operator < (const Point& pt) const { return (pt.vl < vl); }
+
+	//static bool CompareDualToDescend(const Point& p1, const Point& p2)
+	//{
+	//	return VlEq(p1.vl, p2.vl) ? p1.tm < p2.tm : p1.vl > p2.vl;
+	//}
+
+	//static bool CompareDualToAscent(const Point& p1, const Point& p2)
+	//{
+	//	return VlEq(p1.vl, p2.vl) ? p1.tm < p2.tm : p1.vl < p2.vl;
+	//}
+
+#ifdef _DEBUG
+	void Print() const;
+#endif
+};
 
 
 // 'WaveList' represents list of pair <wave file name><frequency>
 class WaveList
 {
-	class File_freq
+	// 'FileFreq' represents pair <wave file name><frequency>
+	struct FileFreq
 	{
+	private:
 		string	_fname;
 		float	_freq;
 
 	public:
-		// Initializes the created instance
+		// Initializes created instance
 		//	@fname: file name
-		File_freq(const string& fname);
+		FileFreq(const string& fname);
 
 		// for sorting by ascent
-		inline bool operator < (const File_freq& f) const { return (f._freq > _freq); }
+		inline bool operator < (const FileFreq& f) const { return (f._freq > _freq); }
 
-		inline void Print() const { cout << _fname << '\t' << (_freq/1000) << LF; }
+		void Print() const;
 	};
+
+	typedef vector<Point>::const_iterator citer;
 
 	// Simple Moving Average splicer
 	class SMA
@@ -42,17 +70,38 @@ class WaveList
 
 	public:
 		// Constructor
-		//	@halfBase: half of base (subset length)
-		inline SMA(short halfBase) : _size(2 * halfBase + 1), _cnt(0), _sum(0) {}
+		//	@len: length of base (subset length)
+		inline SMA(short len) : _size(len - 1), _cnt(0), _sum(0) {}
 
 		// Add value and return average
 		float Push(float x);
 	};
 
+	// Simple Moving Median splicer
+	class SMM
+	{
+		// At each step of the slip, the splicer reads the values by first vakue iterator,
+		// and gives the median.
+		// Thus, at each step, the end of the external value collection is checked.
 
-	const string ext = "csv";
+		const short	_size;		// sliding subset
+		const citer	_end;		// end of external collection
+		vector<float> _v;		// treated value's subset
 
-	vector<File_freq> files;
+	public:
+		// Constructor
+		//	@halfBase: half of base (subset length)
+		//	@end: 'end' iterator of external collection
+		inline SMM(short halfBase, citer end) : _size(2 * halfBase + 1), _end(end) { _v.reserve(_size); }
+
+		// Fill subset by first value iterator and return median
+		//	it@: an iterator to the first value added
+		float Push(citer it);
+	};
+
+	const string Ext = "csv";
+
+	vector<FileFreq> _files;	// FileFreq collection
 
 	// Fills instance by file's names found in given directory
 	//	@dirName: name of directory
@@ -61,6 +110,7 @@ class WaveList
 	bool GetFiles(const string& dirName, const string& ext);
 
 public:
+	// Creates and initializes list of paires <wave file name><frequency>
 	WaveList(const char* path);
 
 	void Print() const;
@@ -75,11 +125,15 @@ static class FS
 	static bool IsExist(const char* name, int st_mode);
 
 public:
-	// Returns the name ended by slash without checking
-	static string const MakePath(const string& name);
+	// Gets size of file or -1 if file doesn't exist
+	static LLONG Size(const char*);
 
 	// Returns true if directory exists
 	inline static bool IsDirExist(const char* name) { return IsExist(name, S_IFDIR); }
+
+	// Returns the name ended by slash without checking
+	static string const MakePath(const string& name);
+
 } fs;
 
 // 'Timer' measures the single wall time interval
